a4c9d9fe3f74b6d24b2287dbc407eeed378690d9
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2022-09-14 19:46:58 +0200

    nn course done

--- pythonfold/flowers.py
+++ pythonfold/flowers.py
@@ -1,12 +1,7 @@
 import flowersdata as data
 import math
+import random
 
-weights = [[0.1, 0.2], [0.15, 0.25], [0.18, 0.1]]
-biases = [0.3, 0.4, 0.35] 
-epochs = 100
-learning_rate = 0.2
-
-# softmax function
 def softmax(predictions):
     m = max(predictions)
     temp = [math.exp(p - m) for p in predictions]
@@ -17,31 +12,68 @@ def log_loss(activations, targets):
     losses = [-t * math.log(a) - (1 - t) * math.log(1 - a) for a, t in zip(activations, targets)]
     return sum(losses)
 
-# train the network
-for epoch in range(epochs): 
-    pred = [[sum([w * i for w, i in zip(we, inp)]) + bi for we, bi in zip(weights, biases)] for inp in data.inputs]
-    act = [softmax(p) for p in pred]
-    cost = sum([log_loss(ac, ta) for ac, ta in zip(act, data.targets)]) / len(data.targets)
-    print(f"Epoch: {epoch} cost: {cost:.1f}")
+epochs = 2000
+learning_rate = 1.2
+input_count, hidden_count, output_count = 2, 8, 3
 
-    errors_d = [[a - t for a, t in zip(ac, ta)] for ac, ta in zip(act, data.targets)] # errors derivative
-    inputs_T = list(zip(*data.inputs)) # transpose inputs
-    errors_d_T = list(zip(*errors_d)) # transpose errors derivative
-    weights_d = [[sum([e * i for e, i in zip(er, inp)]) for er in errors_d_T] for inp in inputs_T] # weights derivative
-    biases_d = [sum([e for e in errors]) for errors in errors_d_T] # biases derivative
+w_i_h = [[random.random() - 0.5 for _ in range(input_count)] for _ in range(hidden_count)]
+w_h_o = [[random.random() - 0.5 for _ in range(hidden_count)] for _ in range(output_count)]
+b_i_h = [0 for _ in range(hidden_count)]
+b_h_o = [0 for _ in range(output_count)]
 
-    weights_d_T = list(zip(*weights_d)) # transpose weights derivative
-    for y in range(len(weights_d_T)):
-        for x in range(len(weights_d_T[0])):
-            weights[y][x] -= learning_rate * weights_d_T[y][x] / len(data.inputs)
-        biases[y] -= learning_rate * biases_d[y] / len(data.inputs)
+for epoch in range(epochs):
+    pred_h = [[sum([w * a for w, a in zip(weights, inp)]) +
+        bias for weights, bias in zip(w_i_h, b_i_h)] for inp in data.inputs]
+    act_h = [[max(0, p) for p in pred] for pred in pred_h]  # apply ReLU
+    pred_o = [[sum([w * a for w, a in zip(weights, inp)]) +
+        bias for weights, bias in zip(w_h_o, b_h_o)] for inp in act_h]
+    act_o = [softmax(predictions) for predictions in pred_o]
 
+    cost = sum([log_loss(a, t) for a, t in zip(act_o, data.targets)]) / len(act_o)
+    print(f"epoch:{epoch} cost:{cost:.4f}")
+
+    # Error derivatives
+    errors_d_o = [[a - t for a, t in zip(ac, ta)] for ac, ta in zip(act_o, data.targets)]
+    w_h_o_T = list(zip(*w_h_o))
+    errors_d_h = [[sum([d * w for d, w in zip(deltas, weights)]) * (0 if p <= 0 else 1)
+        for weights, p in zip(w_h_o_T, pred)] for deltas, pred in zip(errors_d_o, pred_h)]
+
+    # Gradient hidden->output
+    act_h_T = list(zip(*act_h))
+    errors_d_o_T = list(zip(*errors_d_o))
+    w_h_o_d = [[sum([d * a for d, a in zip(deltas, act)]) for deltas in errors_d_o_T]
+        for act in act_h_T]
+    b_h_o_d = [sum([d for d in deltas]) for deltas in errors_d_o_T]
+
+    # Gradient input->hidden
+    inputs_T = list(zip(*data.inputs))
+    errors_d_h_T = list(zip(*errors_d_h))
+    w_i_h_d = [[sum([d * a for d, a in zip(deltas, act)]) for deltas in errors_d_h_T]
+        for act in inputs_T]
+    b_i_h_d = [sum([d for d in deltas]) for deltas in errors_d_h_T]
+
+    # Update weights and biases for all layers
+    w_h_o_d_T = list(zip(*w_h_o_d))
+    for y in range(output_count):
+        for x in range(hidden_count):
+            w_h_o[y][x] -= learning_rate * w_h_o_d_T[y][x] / len(data.inputs)
+        b_h_o[y] -= learning_rate * b_h_o_d[y] / len(data.inputs)
+
+    w_i_h_d_T = list(zip(*w_i_h_d))
+    for y in range(hidden_count):
+        for x in range(input_count):
+            w_i_h[y][x] -= learning_rate * w_i_h_d_T[y][x] / len(data.inputs)
+        b_i_h[y] -= learning_rate * b_i_h_d[y] / len(data.inputs)
 
 # test the network
-pred = [[sum([w * i for w, i in zip(we, inp)]) + bi for we, bi in zip(weights, biases)] for inp in data.test_inputs]
-act = [softmax(p) for p in pred]
+pred_h = [[sum([w * a for w, a in zip(weights, inp)]) +
+    bias for weights, bias in zip(w_i_h, b_i_h)] for inp in data.test_inputs]
+act_h = [[max(0, p) for p in pre] for pre in pred_h]
+pred_o = [[sum([w * a for w, a in zip(weights, inp)]) + bias
+    for weights, bias in zip(w_h_o, b_h_o)] for inp in act_h]
+act_o = [softmax(predictions) for predictions in pred_o]
 correct = 0
-for a, t in zip(act, data.test_targets):
+for a, t in zip(act_o, data.test_targets):
     if a.index(max(a)) == t.index(max(t)):
         correct += 1
-print(f"Correct: {correct} / {len(data.test_targets)}")
+print(f"Correct: {correct}/{len(act_o)} ({correct / len(act_o):%})")
